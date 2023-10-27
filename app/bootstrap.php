cfb334020cdd19bdc503824df3f82a69bcb72bed
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-27 17:11:34 +0200

    Add config.php to bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,8 @@ spl_autoload_register(function ($className){
     require_once ('../app/libraries/'.$className.'.php');
 });
 
+require_once('../app/config/config.php');
+
 // require_once( '../app/libraries/Controller.php');
 // require_once( '../app/libraries/Core.php');
 // require_once('../app/libraries/Database.php');

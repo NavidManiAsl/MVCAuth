aa23fd17ac6d92b17744cf511a61968002c484f3
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-27 20:40:54 +0200

    Create the Database class to use with the models

--- app/libraries/database.php
+++ app/libraries/database.php
@@ -1,2 +1,34 @@
 <?php
 
+class Database
+{
+    private $Name = DB_NAME;
+    private $User = DB_USER;
+    private $Password = DB_PASSWORD;
+    private $Host = DB_HOST;
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct() 
+    {
+        $dsn = 'mysql:host='. $this->Host . ';dbname='. $this->Name;
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_PERSISTENT => true,
+        ];
+
+        try {
+            $this->dbh = new PDO($dsn, $this->User, $this->Password, $options);
+            
+            
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+
+}
+
+

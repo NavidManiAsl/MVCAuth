4a3fda5c11815b75b93b6ff8134f2cdc93c2318f
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-27 22:28:27 +0200

    Clean up comments

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,6 +6,3 @@ spl_autoload_register(function ($className){
 
 require_once('../app/config/config.php');
 
-// require_once( '../app/libraries/Controller.php');
-// require_once( '../app/libraries/Core.php');
-// require_once('../app/libraries/Database.php');

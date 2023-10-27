49c22cd6c8b91aa57429c7c8a73f27fb930eb605
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-27 19:54:16 +0200

    Parse .env in the config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,22 @@
-<?php 
+<?php
 
-define("APP_ROOT",dirname(dirname(__FILE__)));
+define("APP_ROOT", dirname(dirname(__FILE__)));
 define('URI', 'http://localhost:81/mvc/');
-define('PROJECT_NAME','MVC-Framework');
+define('PROJECT_NAME', 'MVC-Framework');
+
+
+//DB credentials
+
+$envFile = APP_ROOT . "/.env";
+if (file_exists($envFile)) {
+    $dotenv = parse_ini_file($envFile);
+} else {
+    die('.env file not found');
+}
+
+define("DB_USER", $dotenv["DB_USER"]);
+define("DB_PASSWORD", $dotenv["DB_PASSWORD"]);
+define("DB_HOST", $dotenv["DB_HOST"]);
+define("DB_NAME", $dotenv["DB_NAME"]);
+
+

6ac1cd5070e8c40ab5ae2172f2b704757be27113
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-30 20:34:55 +0100

    Clean up dead code

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,24 +1,34 @@
 <?php
+use app\validation\Validator;
 
 class Users extends Controller
 {
     public function register()
     {
-       
+
+        $data = [
+            "username" => "",
+            "email" => "",
+            "password" => "",
+            "password_confirmation" => "",
+            "username_error" => "",
+            "email_error" => "",
+            "password_error" => "",
+            "password_confirmation_error" => "",
+        ];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (Validator::registerDataIsValid($_POST)) {
+                $this->view('users.login');
+            } else {
+                $this->view('users.register', array_merge($data, Validator::registerValidation($_POST)));
+            }
         } else {
-            $data = [
-                "username" => "",
-                "email" => "",
-                "password" => "",
-                "password_confirmation" => "",
-                "username_error" => "",
-                "email_error" => "",
-                "password_error" => "",
-                "password_confirmation_error" => "",
-            ];
             $this->view("users.register", $data);
         }
     }
-    public function login(){$this->view("users.login");}
+
+    public function login()
+    {
+        $this->view("users.login");
+    }
 }

8503c5a118b22aaefec70f59b01a6a9c288416e8
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-31 12:07:59 +0100

    Create user registeration method

--- app/models/User.php
+++ app/models/User.php
@@ -7,6 +7,11 @@ class User
     {
         $this->db = $db;
     }
+   
+   /**
+    * Check if a given email is already exist in database.
+    * @return bool
+    */
     public function findByEmail($email)
     {
         $query = 'SELECT * FROM users WHERE email=:email';
@@ -17,5 +22,25 @@ class User
             return true;
         }
         return false;
+    }
+
+    /**
+     * Create and save a new user in database.
+     * @return bool
+     */
+    public function create($data)
+    {
+        try {
+            $query = ' INSERT INTO  users(username,email,password) VALUES (:username, :email, :password)';
+            $this->db->query($query);
+            $this->db->bind(':username', $data['username']);
+            $this->db->bind(':email', $data['email']);
+            $this->db->bind(':password', password_hash($data['password'], PASSWORD_DEFAULT));
+            
+            return $this->db->execute();
+        } catch (\Throwable $th) {
+           echo $th->getMessage();
+           return false;
+        }
     }
 }

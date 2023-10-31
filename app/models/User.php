a1ae422b8d33bc4834aacceb19036e48c8468aaf
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-31 22:32:56 +0100

    Create login method in user model

--- app/models/User.php
+++ app/models/User.php
@@ -7,11 +7,11 @@ class User
     {
         $this->db = $db;
     }
-   
-   /**
-    * Check if a given email is already exist in database.
-    * @return bool
-    */
+
+    /**
+     * Check if a given email is already exist in database.
+     * @return bool
+     */
     public function findByEmail($email)
     {
         $query = 'SELECT * FROM users WHERE email=:email';
@@ -26,21 +26,45 @@ class User
 
     /**
      * Create and save a new user in database.
+     * @param array $formData
      * @return bool
      */
-    public function create($data)
+    public function create($formData)
     {
         try {
             $query = ' INSERT INTO  users(username,email,password) VALUES (:username, :email, :password)';
             $this->db->query($query);
-            $this->db->bind(':username', $data['username']);
-            $this->db->bind(':email', $data['email']);
-            $this->db->bind(':password', password_hash($data['password'], PASSWORD_DEFAULT));
-            
+            $this->db->bind(':username', $formData['username']);
+            $this->db->bind(':email', $formData['email']);
+            $this->db->bind(':password', password_hash($formData['password'], PASSWORD_DEFAULT));
+
             return $this->db->execute();
         } catch (\Throwable $th) {
-           echo $th->getMessage();
-           return false;
+            echo $th->getMessage();
+            return false;
+        }
+    }
+
+    /**
+     * Authenticate a user with given credentials
+     * @param array $formData
+     * @return bool
+     */
+    public function login($formData)
+    {
+        $query = 'SELECT * FROM users WHERE email=:email';
+        try {
+            $this->db->query($query);
+            $this->db->bind(':email', $formData['email']);
+            $row = $this->db->result();
+            
+            if ($row && password_verify($formData['password'], $row->password)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (\Throwable $th) {
+           
         }
     }
 }

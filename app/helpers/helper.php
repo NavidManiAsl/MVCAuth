f97637f202d6ba5a06ad77703e2c62244ec85ce2
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-31 14:43:14 +0100

    Create flash message function

--- app/helpers/helper.php
+++ app/helpers/helper.php
@@ -1,10 +1,36 @@
 <?php
+session_start();
 
 /** 
  * Rediret to a certain page
+ * @param string $page
  * @return void
  */
 function redirect($page)
 {
     header('location:' . URI . $page);
+}
+
+/**
+ * displays a flash message.
+ * @param string $name 
+ * @param string|null $message
+ * @return void;
+ */
+function flash($name, $message = null)
+{
+    if (!empty($name) && !empty($message)) {
+        if (!empty($_SESSION[$name])) {
+            unset($_SESSION[$name]);
+            
+        }
+        
+        $_SESSION[$name] = $message;
+    }
+
+
+    if (!empty($_SESSION[$name]) && !empty($name) && empty($message)) {
+        echo ('<div id="flash-msg">' . $_SESSION[$name] . '</div>');
+        unset($_SESSION[$name]);
+    }
 }

ef56a030196c34cdee8480133c0c7762c6af227d
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-04 18:57:00 +0100

    Create  autoload for helper directory

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,10 @@ spl_autoload_register(function ($className){
     require_once ('../app/libraries/'.$className.'.php');
 });
 
+spl_autoload_register(function ($className){
+    require_once ('../app/helpers'.$className.'.php');
+});
+
 require_once('../app/config/config.php');
-require_once('../app/validation/Validator.php');
-require_once('../app/helpers/helper.php');
+
 

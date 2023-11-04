37731ed18daba83c9970975263344a3ca43909f8
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-04 20:04:23 +0100

    Refactor bootstrap autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,13 +1,14 @@
 <?php
 
-spl_autoload_register(function ($className){
-    require_once ('../app/libraries/'.$className.'.php');
-});
+spl_autoload_register(function ($className) {
 
-spl_autoload_register(function ($className){
-    require_once ('../app/helpers'.$className.'.php');
+    if (file_exists('../app/libraries/' . $className . '.php')) {
+        require_once('../app/libraries/' . $className . '.php');
+    } else {
+        require_once('../app/helpers/' . $className . '.php');
+    }
 });
 
 require_once('../app/config/config.php');
-
+require_once('../app/helpers/helper.php');
 

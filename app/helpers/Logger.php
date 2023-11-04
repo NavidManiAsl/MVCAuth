5ad5897af23379729488fe5ee082e306575b60f8
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-04 22:16:52 +0100

    Create log method

--- app/helpers/Logger.php
+++ app/helpers/Logger.php
@@ -1,6 +1,18 @@
 <?php
 
-class Logger 
+class Logger
 {
-    
+
+    /** 
+     * Logs a string to a json file.
+     * @param array $message
+     * @param string $logFile
+     * @return void
+     */
+    public static function log($message, $logFile)
+    {
+        $log = array_merge(array('timestamp' => date('Y-m-d H:i:s', time())), $message);
+        $jsonLog = json_encode($log);
+        file_put_contents(LOGS . $logFile, $jsonLog . PHP_EOL, FILE_APPEND);
+    }
 }

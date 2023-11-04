959211eedd11835c14f840fb7757808838de8cee
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-04 23:39:59 +0100

    Create handle error method

--- app/helpers/ErrorHandler.php
+++ app/helpers/ErrorHandler.php
@@ -1,5 +1,32 @@
 <?php
 
-class ErrorHandler {
-    
+class ErrorHandler
+{
+
+    /**
+     * Handle and extracts the data of an error object and logs it.
+     * @param Throwable $throwable
+     * @param int $code
+     * @param string $message
+     * @return array
+     */
+    public static function handleError(\Throwable $throwable, $code = null, $message = null)
+    {
+        $date = date("Y-m-d H:i:s", time());
+        $description = $throwable->getMessage();
+        $file = $throwable->getFile();
+        $line = $throwable->getLine();
+        $trace = $throwable->getTraceAsString();
+
+        $th = [$date, $description, $file, $line, $trace];
+        Logger::log($th, "error");
+
+        return [
+            'code' => $code ? $code : 500,
+            'message' => $message ? $message : 'Something went wrong, please try again later.'
+        ];
+
+    }
+
+
 }

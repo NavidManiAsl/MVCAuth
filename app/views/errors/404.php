70375eac42039a0dd66d740c6b23d432fe6fc45f
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-05 23:00:31 +0100

    Create 404 500 views

---
+++ app/views/errors/404.php
@@ -0,0 +1,7 @@
+<?php
+require_once(APP_ROOT.'/views/partials/header.php');
+?>
+<h1>404</h1>
+<?php
+require_once(APP_ROOT.'/views/partials/footer.php');
+?>
---
+++ app/views/errors/500.php
@@ -0,0 +1,3 @@
+<?php require_once(APP_ROOT."/views/partials/header.php");?>
+<h1>500</h1>
+<?php require_once(APP_ROOT."/views/partials/footer.php");?>

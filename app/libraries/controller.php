0ca63cee649828afac708b09fa828649c24a946e
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-06 20:22:18 +0100

    Add comment blocks and error handling

--- app/libraries/controller.php
+++ app/libraries/controller.php
@@ -1,32 +1,50 @@
 <?php
 namespace App\Libraries;
+
+use App\Helpers\ErrorHandler;
+
 class Controller
 {
 
-    protected function model($model , $dependencies = null)
+    /**
+     * loads a model file instantiate it and inject the provided dependencies.
+     * @param string $model
+     * @param array $dependencies
+     * @return object|void
+     */
+    protected function model($model, $dependencies = [])
     {
-        $model = ucfirst($model);
-        if (file_exists(APP_ROOT.'/models/' . ucfirst($model) . '.php')) {
-           
-            require_once(APP_ROOT . '/models/' . ucfirst($model) . '.php');
-                
+        try {
+            $model = ucfirst($model);
+            if (file_exists(APP_ROOT . '/models/' . ucfirst($model) . '.php')) {
+
+                require_once(APP_ROOT . '/models/' . ucfirst($model) . '.php');
+
                 $class = 'App\\Models\\' . $model;
                 return new $class($dependencies);
-            
-        }else{ }
+
+            } else {
+                throw new \Exception('Model not found.');
+            }
+        } catch (\Exception $e) {
+            ErrorHandler::handleError($e);
+        }
     }
 
-    protected function view($view,$data=[])
+    /**
+     * Loads and renders a specified view file.
+     * @param string $view
+     * @param array $data
+     * @return void
+     */
+    protected function view($view, $data = [])
     {
-        $view=str_replace('.','/', $view);
+        $view = str_replace('.', '/', $view);
 
-        if (file_exists( '../app/views/'. $view . '.php')) {
+        if (file_exists('../app/views/' . $view . '.php')) {
 
-            require_once('../app/views/'. $view . '.php');
-            
+            require_once('../app/views/' . $view . '.php');
+
         }
-        
-        
-        
     }
 }

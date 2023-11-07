0f901e9acd648d3219f80c977a2dc67de330ade9
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-07 21:45:42 +0100

    update the .env.example and config

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
-define("APP_ROOT", dirname(dirname(__FILE__)));
+define("APP_ROOT", dirname(dirname(__FILE__))); //is the directory address where your application files are located.
 define('PROJECT_NAME', 'MVC-Auth');
-define('LOGS',dirname(dirname(__FILE__)).'\..\logs\\');
+define('LOGS',dirname(dirname(__FILE__)).'\..\logs\\'); // is the directory address where your logs will be stored.
+define("SESSION_LIFE_SPAN",1800);
 
 
-//DB credentials
 
 $envFile = APP_ROOT . "/.env";
 if (file_exists($envFile)) {
@@ -15,10 +15,10 @@ if (file_exists($envFile)) {
 }
 
 define('URI', $dotenv["BASE_URL"]);
+//DB credentials
 define("DB_USER", $dotenv["DB_USER"]);
 define("DB_PASSWORD", $dotenv["DB_PASSWORD"]);
 define("DB_HOST", $dotenv["DB_HOST"]);
 define("DB_NAME", $dotenv["DB_NAME"]);
-define("SESSION_LIFE_SPAN",1800);
 
 
